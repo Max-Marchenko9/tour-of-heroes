a8fef18a00af1d99b61c7c36a0004a33baf68ab1
Max-Marchenko9 <83778958+Max-Marchenko9@users.noreply.github.com>
2021-08-22 21:17:24 +0300

    Get data from a server #1

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,22 +3,27 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heres';
 import { Observable, of } from 'rxjs';
 import { MessagesService } from './messages.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  constructor(private messageService: MessagesService) { }
+
+  private heroesUrl = 'api/heroes';
+
+  constructor(private http: HttpClient,
+              private messageService: MessagesService) {}
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
   getHero(id: number): Observable<Hero>{
     const hero = HEROES.find(h => h.id === id)!;
     this.messageService.add('HeroService: fetched hero id=${id}');
     return of(hero);
   }
-
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`)}
 }
