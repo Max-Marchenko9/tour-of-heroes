9e09b6392f9f9fbf2fe521b9e89766cf3f4ae2ba
Max-Marchenko9 <83778958+Max-Marchenko9@users.noreply.github.com>
2021-08-22 19:12:33 +0300

    Add Navigation full

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -14,10 +14,10 @@ export class DashboardComponent implements OnInit {
   constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
   getHeroes():void {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes.slice(1, 5) )
   }
-
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,18 +10,12 @@ import { MessagesService } from '../messages.service';
 })
 export class HeroesComponent implements OnInit {
 
-  selectedHero?: Hero
   heroes: Hero[] = [];
   
-  constructor(private heroService: HeroService,
-              private messageService: MessagesService) { }
+  constructor(private heroService: HeroService,) { }
   
   ngOnInit(): void {
     this.getHeroes();
-  }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: selected hero id=${hero.id}`);
   }
   getHeroes(): void {
     this.heroService.getHeroes()
